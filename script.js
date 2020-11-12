687cfeb18cd633ad3ffdc1819a9c8c6af7a4c9c6
HaqpaHR <pereverzev.viacheslav@gmail.com>
2020-11-12 12:55:41 +0200

    first step

---
+++ script.js
@@ -0,0 +1,29 @@
+const add = document.querySelector("#add");
+
+add.textContent = "Hello world";
+
+console.log("hello");
+
+const good = document.querySelector("#delete");
+
+good.textContent = "HELLYEAH";
+
+console.error("Hell");
+
+const info = document.querySelector("#info");
+
+info.textContent = "What is love";
+
+console.info("Here is info");
+
+const table = document.querySelector("#table");
+
+table.textContent = "Baby don't hurt me";
+
+console.table("here is table")
+
+const clear = document.querySelector("#clear");
+
+clear.textContent = "no more";
+
+console.clear("clear")
